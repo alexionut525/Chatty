c9d6088a9c46b71d03d6b584a4d766c027a8e924
alexionut525 <alexionut525@gmail.com>
2019-04-03 00:26:42 +0300

    03.04.2019
    Changes:
    -user info.
    -auto login
    -logout

--- app/src/main/java/com/alexspataru/chatty/StartActivity.java
+++ app/src/main/java/com/alexspataru/chatty/StartActivity.java
@@ -6,14 +6,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class StartActivity extends AppCompatActivity {
 
+    FirebaseUser firebaseUser;
     Button login,register;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if(firebaseUser != null){
+
+            Intent intent = new Intent( StartActivity.this, MainActivity.class );
+            startActivity(intent);
+            finish();
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_start );
+
 
         login = findViewById( R.id.login );
         register = findViewById( R.id.register );
@@ -24,7 +44,13 @@ public class StartActivity extends AppCompatActivity {
                 startActivity(new Intent(StartActivity.this,LoginActivity.class));
             }
         } );
-            startActivity( new Intent( StartActivity.this,RegisterActivity.class ));
+        register.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity( new Intent( StartActivity.this,RegisterActivity.class ));
+            }
+        } );
+
     }
 
 
